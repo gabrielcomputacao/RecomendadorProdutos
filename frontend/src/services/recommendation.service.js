5d5c4a1c25915b1269a3fc753ff154cf49235357
Gabriel <gabrielcomputacao@outlook.com>
2025-05-07 19:39:48 -0300

    feat: Criando função para retornar produtos recomendados

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -1,5 +1,15 @@
 // getRecommendations.js
 
+import { normalizeText } from '../utils/normalizeText';
+
+const hasMatch = (arrayProduct, arrayForm) => {
+  if (!arrayProduct.length || !arrayForm.length) return false;
+
+  return arrayProduct.some((product) =>
+    arrayForm.some((formProduct) => formProduct === normalizeText(product))
+  );
+};
+
 const getRecommendations = (
   formData = { selectedPreferences: [], selectedFeatures: [] },
   products
@@ -7,6 +17,30 @@ const getRecommendations = (
   /**
    * Crie aqui a lógica para retornar os produtos recomendados.
    */
+
+  console.log(formData)
+
+  const selectedPreferencesNormalized = formData.selectedPreferences.map(
+    (preference) => normalizeText(preference)
+  );
+  const selectedFeaturesNormalized = formData.selectedFeatures.map((feature) =>
+    normalizeText(feature)
+  );
+
+  const selectedProducts = products.filter((product) => {
+    const matchPreference =
+      !selectedPreferencesNormalized ||
+      hasMatch(product.preferences, selectedPreferencesNormalized);
+
+    const matchFeature =
+      !selectedFeaturesNormalized ||
+      hasMatch(product.features, selectedFeaturesNormalized);
+
+    return matchFeature || matchPreference;
+  });
+
+    return selectedProducts;
+
 };
 
 export default { getRecommendations };
